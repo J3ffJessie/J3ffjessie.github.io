8ed38ed9ceaa1673289a1e87d1023d8b78a217ce
J3ffJessie <jejess6125@gmail.com>
2020-01-15 13:24:12 -0600

    Fixed navigation bar, fixed Projects screen, added link to Blog, still working on adding all projects that I want to be listed in the Projects section

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// toggle script for the navbar
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
-});
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+};
