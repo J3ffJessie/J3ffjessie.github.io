b18e962c9cd843d2605128aa790de3203d52499d
Jeff Jessie <j3ffjessie@icloud.com>
2020-05-17 21:19:57 -0500

    Revert changes as Contact area is causing other issues. Revert back until I can fix the bug.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function myFunction() {
   }
 };
 
+
+// Typed Heading
+
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -63,6 +66,7 @@ window.onload = function() {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
 
 
 
