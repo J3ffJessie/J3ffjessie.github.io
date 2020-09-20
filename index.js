2ab3bf6351dd13a59bf12eed5448e74291ae56ff
Jeff Jessie <jejess6125@gmail.com>
2020-09-20 17:26:57 -0500

    Style changes and layout changes.

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-//header scrolling effect
 $(window).on('scroll', function() {
-  if($(window).scrollTop()) {
-      $('header').addClass('nav-show');
+  if($(window).scrollTop()){
+    $('header').addClass('nav-show');
   } else {
-      $('header').removeClass('nav-show');
+    $('header').removeClass('nav-show');
   }
 })
 
-//hamburger
 const navSlide = () => {
   const hamburger = document.querySelector(".hamburger");
   const navbar = document.querySelector(".nav-bar");
-  const navlinks = document.querySelector(".nav-bar li");
+  const navLinks = document.querySelector(".nav-bar li");
 
-  hamburger.onClick = () => {
-      navbar.classList.toggle("nav-active");
+  hamburger.onclick = () => {
+    navbar.classList.toggle("nav-active");
 
-      //Animation Links
-      navLinks.forEach((link, index) => {
-          if(link.style.animation) {
-              link.style.animation = "";
-          } else {
-              link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7+1}s';
-          }
-      });
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+      }
+    });
+    hamburger.classList.toggle("toggle");
+
   }
 }
-
 
 window.onload = () => navSlide();
