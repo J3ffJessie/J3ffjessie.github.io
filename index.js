82cdb30356a512495277d95a222ff5950bc63780
Jeff Jessie <jejess6125@gmail.com>
2020-09-20 21:42:06 -0500

    Add toggle for dark/light theme. Add changes to css variables in function and test.

--- index.js
+++ index.js
@@ -27,8 +27,19 @@ const navSlide = () => {
 }
 
 // use this for changing variable colors and making the theme light on click of toggle
-// const themeSwitch = () =>{
-//   document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');
+function lightTheme(){
+  document.documentElement.style.setProperty('--main-background', '#717788');
+  document.documentElement.style.setProperty('--main-fonts-color', '#000000');
+  document.documentElement.style.setProperty('--main-decor-color', '#f9f871');
+  document.documentElement.style.setProperty('--main-header-background','#d9a21b');
+};
+
+
+// function darkTheme() {
+//   document.documentElement.style.setProperty('--main-background', '#0b0c0f');
+//   document.documentElement.style.setProperty('--main-fonts-color', '#fff');
+//   document.documentElement.style.setProperty('--main-decor-color', '#00a9e2'));
+//   document.documentElement.style.setProperty('--main-header-background', '#21252e');
 // }
 
 
