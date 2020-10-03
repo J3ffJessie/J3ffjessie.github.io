185f17759b5a357a9c3eaf0620aadc5a50ef319b
Jeff Jessie <jejess6125@gmail.com>
2020-10-03 18:42:50 -0500

    Functioning Dark mode toggle, Selecting light mode colors and adding projects soon.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const navSlide = () => {
     hamburger.classList.toggle("toggle");
 
   }
-}
+};
 
 
 let theme = true;
@@ -40,10 +40,10 @@ function changeTheme(){
 
 // use this for changing variable colors and making the theme light on click of toggle
 function lightTheme() {
-  document.documentElement.style.setProperty('--main-background', '#717788');
-  document.documentElement.style.setProperty('--main-fonts-color', '#000000');
-  document.documentElement.style.setProperty('--main-decor-color', '#f9f871');
-  document.documentElement.style.setProperty('--main-header-background', '#d9a21b');
+  document.documentElement.style.setProperty('--main-background', '#d3d6df');
+  document.documentElement.style.setProperty('--main-fonts-color', 'black');
+  document.documentElement.style.setProperty('--main-decor-color', '#00b1ec');
+  document.documentElement.style.setProperty('--main-header-background', '#bbc1cf');
 };
 
 function darkTheme() {
